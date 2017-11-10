d256d84b4491ef11a77569ac10227a4c4def09b0
KiKu25 <kristersleinerts@gmail.com>
2017-11-10 21:39:38 +0200

    Episode 1

---
+++ Hex/Assets/Hex.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The Hex class defines the grid position, world space position, size,
+/// neighbours, etc... of a Hex Tile. However, it does NOT interact with 
+/// Unity directly in any way
+/// </summary>
+public class Hex  {
+
+    
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+    }
+
+    // Q + R + S = 0
+    // S = -(Q + R)
+
+    public readonly int Q;  // Column
+    public readonly int R;  // Row
+    public readonly int S;  // Summ
+
+    static readonly float WIFDTH_MULTILIER = Mathf.Sqrt(3) / 2;
+
+    /// <summary>
+    /// Returns the world-space psoition of this hex
+    /// </summary>
+    /// <returns>Position</returns>
+    public Vector3 Position()
+    {
+        float radius = 1f;
+        float height = radius * 2;
+        float wight = WIFDTH_MULTILIER * height;
+
+        float vert = height * 0.75f;
+        float horiz = wight;
+
+        return new Vector3(horiz * (Q + R/2f), 0, vert * R);
+    }
+}
---
+++ Hex/Assets/HexMap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour {
+
+    public GameObject HexPrefab;
+
+
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+	}
+	
+	public void GenerateMap()
+    {
+        for (int colum = 0; colum < 10; colum++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                Hex hex = new Hex(colum, row);
+
+                Instantiate(HexPrefab, hex.Position(), Quaternion.identity, this.transform);
+            }
+        }
+    }
+}
